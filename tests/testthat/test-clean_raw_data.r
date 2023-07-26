774e3d3ae8ca842ca441932daaea50b955c9609d
JavierKiller <a205201460@unison.mx>
2023-07-25 21:18:32 -0700

    firts_test-clean_raw_data

--- tests/testthat/test-clean_raw_data.r
+++ tests/testthat/test-clean_raw_data.r
@@ -1,5 +1,11 @@
+path_data_prefix <- "~/CursoQR/Package1/rStegomyiaIndices/rStegomyiaIndices"
+path_data_file_name <- "/data/qr_for_test.csv"
+path_data <- paste(path_data_prefix, path_data_file_name, sep = "")
+df <- read.csv(path_data)
 test_that("multiplication works", {
   expected <- clean_raw_data(df)
+  expect_s3_class(expected, "data.frame")
+  expect_equal(dim(expected), c(20, 12))
   # TODO provar tipos de datos  de colt ####
   #TODO porbar tipos de datos de subconjuntos de colt ####
   #TODO probar archivos de salida .csv para colt ####
