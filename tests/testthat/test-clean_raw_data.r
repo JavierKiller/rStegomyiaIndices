99edf62f38fa98e55e62becae1998b3256a89142
JavierKiller <a205201460@unison.mx>
2023-07-30 20:28:12 -0700

    firt_test_sub_clean_raw_data

--- tests/testthat/test-clean_raw_data.r
+++ tests/testthat/test-clean_raw_data.r
@@ -18,12 +18,30 @@ colt= list(
   Total_de_Recipientes_con_Agua = "d",
   Total_de_Recipientes_Positivos = "d"
 )
+colt_d= list(
+  Tipo_de_Estudio = col_factor(c("Encuesta", "Verificacion")),
+  Fecha_de_Inicio = col_date(format = "%d/%m/%Y"),
+  Semana_Epidemiologica = "f",
+  Casas_Revisadas = "d",
+  Casas_Positivas = "d",
+  Total_de_Recipientes_con_Agua = "d",
+  Total_de_Recipientes_Positivos = "d"
+)
 test_that("assess_clean_raw_data", {
   expected <- clean_raw_data(df)
   expect_identical(str(colt), str(expected))
   expect_s3_class(expected, "data.frame")
   expect_equal(dim(expected), c(20, 12))
-  #TODO porbar tipos de datos de subconjuntos de colt ####
   #TODO probar archivos de salida .csv para colt ####
   #TODO probar archivos de salida .csv para subconjunto de colt ####
  })
+#TODO porbar tipos de datos de subconjuntos de colt ####
+test_that("assess_clean_raw_data_sub", {
+  expected <- clean_raw_data(df, colt= colt_d )
+  expect_identical(str(colt_d), str(expected))
+  expect_s3_class(expected, "data.frame")
+  expect_equal(dim(expected), c(20, 7))
+
+  #TODO probar archivos de salida .csv para colt ####
+  #TODO probar archivos de salida .csv para subconjunto de colt ####
+})
